2251bd885eaf164cb81a32a43134807162eca253
Iana Sanduta <iana.sandutsa@gmail.com>
2022-02-22 22:23:27 -0700

    Api merged in the master

---
+++ lib/models/food.api.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:nearcals/models/food.dart';
+import 'package:http/http.dart' as http;
+
+class FoodApi {
+  static Future<List<Food>> getcalories() async {
+    var uri = Uri.https('api.nutritionix.com', '/v1_1/search/',
+        {"fields": "item_name,brand_name,nf_calories"});
+
+    final response = await http.get(uri, headers: {
+      "appId":"bd3f7a80",
+      "x-rapidapi-key": "43bb77efc418470e481a38f9f73b0891",
+      "useQueryString": "true"
+    });
+    print(response.body);
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['hits']) {
+      _temp.add(i['fields']);
+    }
+
+    return Food.caloriesFromSnapshot(_temp);
+  }
+}
---
+++ lib/views/widgets/recipe_card.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class RecipeCard extends StatelessWidget {
+  final String name;
+  final String calories;
+  final String brandName;
+  RecipeCard({
+    required this.name,
+    //required this.cookTime,
+    required this.calories,
+    required this.brandName,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+      width: MediaQuery.of(context).size.width,
+      height: 180,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.6),
+            offset: Offset(
+              0.0,
+              10.0,
+            ),
+            blurRadius: 10.0,
+            spreadRadius: -6.0,
+          ),
+        ],
+      ),
+      child: Stack(
+        children: [
+          Align(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0),
+              child: Text(
+                name,
+                style: TextStyle(
+                  fontSize: 19,
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            alignment: Alignment.center,
+          ),
+          Align(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(5),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.4),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.yellow,
+                        size: 18,
+                      ),
+                      SizedBox(width: 7),
+                      Text(calories),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(5),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.4),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.schedule,
+                        color: Colors.yellow,
+                        size: 18,
+                      ),
+                      SizedBox(width: 7),
+                      //Text(cookTime),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            alignment: Alignment.bottomLeft,
+          ),
+        ],
+      ),
+    );
+  }
+}
