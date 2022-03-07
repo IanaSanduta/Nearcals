c7a27c1a532f671d516b4dfeb0d50c36dd9e154d
JNope <91224053+JNope01@users.noreply.github.com>
2022-03-07 15:27:03 -0700

    fixed logout issue

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,7 +2,7 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:nearcals/net/userData.dart';
+import 'package:nearcals/classes/userClass.dart';
 import 'package:nearcals/profile.dart';
 import 'package:nearcals/register.dart';
 import 'main.dart';
@@ -16,8 +16,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   //Define authentication function
 
-  void authLongOut() async {
-    await FirebaseAuth.instance.signOut();
+  void authLongOut() {
+    currentUser.clearUser();
     Navigator.push(
         context, MaterialPageRoute(builder: (context) => const Home()));
   }
--- lib/login.dart
+++ lib/login.dart
@@ -12,8 +12,8 @@ class _LoginScreenState extends State<LoginScreen> {
   //Define variables
   bool _isObscure = true;
   final loginEmailController = TextEditingController()
-    ..text = "admin@gmail.com";
-  final loginPassController = TextEditingController()..text = "123456";
+    ..text = "jnope25@gmail.com";
+  final loginPassController = TextEditingController()..text = "Password@1";
 
   //Define authentication function
   void authLongIn() async {
