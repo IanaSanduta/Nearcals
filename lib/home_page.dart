bf05fdadd568d20f6bd6a4bf0d00236ca5cdd828
daniel.arena <daniel.arena@octanner.com>
2022-03-14 13:31:27 -0600

    Webhook

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -35,87 +35,89 @@ class _HomePageState extends State<HomePage> {
     userEmail ??= FirebaseAuth.instance.currentUser?.email;
     userName ??= run;
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home'),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              UserAccountsDrawerHeader(
-                accountName: Text(userName),
-                accountEmail: Text(userEmail!),
-                currentAccountPicture: CircleAvatar(
-                  child: ClipOval(
-                    child: Image.asset(
-                      'resources/default_user.png',
-                      width: 90,
-                      height: 90,
-                      fit: BoxFit.cover,
-                    ),
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text(userName),
+              accountEmail: Text(userEmail!),
+              currentAccountPicture: CircleAvatar(
+                child: ClipOval(
+                  child: Image.asset(
+                    'resources/default_user.png',
+                    width: 90,
+                    height: 90,
+                    fit: BoxFit.cover,
                   ),
                 ),
-                decoration: BoxDecoration(
-                  color: Colors.blue.shade900,
-                  image: const DecorationImage(image: AssetImage('resources/drawer.jpg'), fit: BoxFit.cover),
-                ),
               ),
-              ListTile(
-                iconColor: Colors.blue.shade900,
-                leading: const Icon(Icons.person),
-                title: const Text('Profile'),
-                onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Profile()),
-                ),
+              decoration: BoxDecoration(
+                color: Colors.blue.shade900,
+                image: const DecorationImage(
+                    image: AssetImage('resources/drawer.jpg'),
+                    fit: BoxFit.cover),
               ),
-              ListTile(
-                iconColor: Colors.blue.shade900,
-                leading: const Icon(Icons.map),
-                title: const Text('Map'),
-                onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Maps()),
-                ),
+            ),
+            ListTile(
+              iconColor: Colors.blue.shade900,
+              leading: const Icon(Icons.person),
+              title: const Text('Profile'),
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Profile()),
               ),
-              ListTile(
-                iconColor: Colors.blue.shade900,
-                leading: const Icon(Icons.local_dining),
-                title: const Text('Calories'),
-                onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Calories()),
-                ),
+            ),
+            ListTile(
+              iconColor: Colors.blue.shade900,
+              leading: const Icon(Icons.map),
+              title: const Text('Map'),
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Maps()),
               ),
-              ListTile(
-                iconColor: Colors.blue.shade900,
-                leading: const Icon(Icons.favorite),
-                title: const Text('Favorites'),
-                onTap: () => favListTest(),
+            ),
+            ListTile(
+              iconColor: Colors.blue.shade900,
+              leading: const Icon(Icons.local_dining),
+              title: const Text('Calories'),
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Calories()),
               ),
-              const Divider(),
-              ListTile(
-                iconColor: Colors.blue.shade900,
-                leading: const Icon(Icons.settings),
-                title: const Text('Settings'),
-                onTap: () => print('Settings'),
-              ),
-              ListTile(
-                iconColor: Colors.blue.shade900,
-                leading: const Icon(Icons.description),
-                title: const Text('Guide'),
-                onTap: () => print('guide'),
-              ),
-              const Divider(),
-              ListTile(
-                iconColor: Colors.blue.shade900,
-                leading: const Icon(Icons.exit_to_app),
-                title: const Text('Exit'),
-                onTap: authLongOut,
-              ),
-            ],
-          ),
+            ),
+            ListTile(
+              iconColor: Colors.blue.shade900,
+              leading: const Icon(Icons.favorite),
+              title: const Text('Favorites'),
+              onTap: () => favListTest(),
+            ),
+            const Divider(),
+            ListTile(
+              iconColor: Colors.blue.shade900,
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () => print('Settings'),
+            ),
+            ListTile(
+              iconColor: Colors.blue.shade900,
+              leading: const Icon(Icons.description),
+              title: const Text('Guide'),
+              onTap: () => print('guide'),
+            ),
+            const Divider(),
+            ListTile(
+              iconColor: Colors.blue.shade900,
+              leading: const Icon(Icons.exit_to_app),
+              title: const Text('Exit'),
+              onTap: authLongOut,
+            ),
+          ],
         ),
+      ),
     );
   }
 }
