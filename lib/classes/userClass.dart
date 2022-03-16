198eaea5458b862fd1df386905ea4ddfe76cdacd
JNope <91224053+JNope01@users.noreply.github.com>
2022-03-16 13:37:51 -0600

    slimmer code

--- lib/classes/userClass.dart
+++ lib/classes/userClass.dart
@@ -175,31 +175,14 @@ Future<void> pullUserData() async {
   currentUser.dailyCals = null;
   currentUser.currentCals = null;
   currentUser.favoritesList?.clear();
-
-  while (currentUser.getUserName() == null) {
-    currentUser.pullUserName(data[dbList[0]] as String);
-  }
-  print(currentUser.getUserName());
-  while (currentUser.getDailyCals() == null) {
-    currentUser.pullDailyCals(data[dbList[1]] as int);
-  }
-  print(currentUser.getDailyCals());
-  while (currentUser.getCurrentCals() == null) {
-    currentUser.pullCurrentCals(data[dbList[2]] as int);
-  }
-  print(currentUser.getCurrentCals());
-  while (currentUser.getEmail() == null) {
-    currentUser.pullEmail(data[dbList[3]] as String);
-  }
-  print(currentUser.getEmail());
-  while (currentUser.getUserImage() == null) {
-    currentUser.pullUserImage(data[dbList[4]] as String);
-  }
-  print(currentUser.getUserImage());
-  while (currentUser.getFavoriteList() == null) {
-    currentUser.pullFavoritesList(data[dbList[5]] as Map);
-  }
-  print(currentUser.getFavoriteList());
+  currentUser.pullUserName(data[dbList[0]] as String);
+  currentUser.pullDailyCals(data[dbList[1]] as int);
+  currentUser.pullCurrentCals(data[dbList[2]] as int);
+  currentUser.pullEmail(data[dbList[3]] as String);
+  currentUser.pullFavoritesList(data[dbList[5]] as Map);
+  currentUser.pullUserImage(data[dbList[4]] as String);
+  //TODO:Remove in final
+  checkUserData();
 }
 
 // checkUserData Used to check all the values in currentUser class (not really used)
