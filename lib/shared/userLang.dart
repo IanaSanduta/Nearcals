2cd0b57cc485f73f0bc81e3837d1e41c2440cab8
JNope <91224053+JNope01@users.noreply.github.com>
2022-03-23 15:58:44 -0600

    add lang

---
+++ lib/shared/userLang.dart
@@ -0,0 +1,68 @@
+// ignore_for_file: file_names
+
+import 'package:nearcals/classes/userClass.dart';
+
+String text(String word) {
+  String result = '';
+  final List langList = ['English', 'Spanish'];
+  final Map<String, Map<String, String>> langLib = {
+    //English
+    langList[0]: {
+      'Home': 'Home',
+      'Profile': 'Profile',
+      'Map': 'Map',
+      'Calories': 'Calories',
+      'Favorites': 'Favorites',
+      'Settings': 'Settings',
+      'Guide': 'Guide',
+      'Logout': 'Logout',
+      'Email': 'Email',
+      'Username': 'Username',
+      'Daily Calories': 'Daily Calories',
+      'Save': 'Save',
+      'Foodie Map': 'Foodie Map',
+      'Sign Up': 'Sign Up',
+      'Login': 'Login'
+    },
+    //Spanish
+    langList[1]: {
+      'Home': '.',
+      'Profile': '.',
+      'Map': '.',
+      'Calories': '.',
+      'Favorites': '.',
+      'Settings': '.',
+      'Guide': '.',
+      'Logout': '.',
+      'Email': '.',
+      'Username': '.',
+      'Daily Calories': '.',
+      'Save': '.',
+      'Foodie Map': '.',
+      'Sign Up': '.',
+      'Login': '.'
+    },
+    //Do Not delet rows below its used as a guide to add more languages.
+    /*
+    langList[.]: {
+      'Home': '.',
+      'Profile': '.',
+      'Map': '.',
+      'Calories': '.',
+      'Favorites': '.',
+      'Settings': '.',
+      'Guide': '.',
+      'Logout': '.',
+      'Email': '.',
+      'Username': '.',
+      'Daily Calories':'.',
+      'Save':'.',
+      'Foodie Map': '.',
+      'Sign Up':'.',
+      'Login': '.'
+    }*/
+  };
+  Map<String, String>? languageLibrary = langLib[currentUser.getUserLang()];
+  result = languageLibrary![word] as String;
+  return result;
+}
