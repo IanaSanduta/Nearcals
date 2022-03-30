90b5f71413c29d2fc78ae6faa93b8ded16f6b10d
Iana Sanduta <iana.sandutsa@gmail.com>
2022-03-30 13:48:43 -0600

    Food Api update

--- lib/models/food.api.dart
+++ lib/models/food.api.dart
@@ -6,6 +6,63 @@ import 'package:http/http.dart' as http;
 
 class FoodApi {
   static Future<List<Food>> getcalories() async {
+    var uri = Uri.https('trackapi.nutritionix.com', '/v2/search/instant',
+        {"branded": "food_name,brand_name,nf_calories"});
+
+    final response = await http.get(uri, headers: {
+      "x-app-id": "bd3f7a80",
+      "x-app-key": "43bb77efc418470e481a38f9f73b0891",
+      "useQueryString": "true"
+    });
+    print(response.body);
+
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['branded']) {
+      _temp.add(i['food_name']['brand_name']['nf_calories']);
+    }
+    print(_temp.length);
+
+    return Food.caloriesFromSnapshot(_temp);
+  }
+
+  static Future<List<Food>> getlocation() async {
+    var uri = Uri.https('trackapi.nutritionix.com', '/v2/locations',
+        {"locations": "name,address,distance_km"});
+
+    final response = await http.get(uri, headers: {
+      "x-app-id": "bd3f7a80",
+      "x-app-key": "43bb77efc418470e481a38f9f73b0891",
+      "useQueryString": "true"
+    });
+    print(response.body);
+
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+/*
+    for (var i in data['branded']) {
+      _temp.add(i['food_name']['brand_name']['nf_calories']);
+    }
+
+ */
+    print(_temp.length);
+
+    return Food.caloriesFromSnapshot(_temp);
+  }
+}
+
+
+
+
+
+
+
+/*
+class FoodApi {
+  static Future<List<Food>> getcalories() async {
     var uri = Uri.https('api.nutritionix.com', '/v1_1/search/',
         {"fields": "item_name,brand_name,nf_calories"});
 
@@ -28,3 +85,4 @@ class FoodApi {
     return Food.caloriesFromSnapshot(_temp);
   }
 }
+*/
